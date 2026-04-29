25c48266d764a72ffb15264d1bd44c16f5d057b7
21-firdaws <firdaushussein56@gmail.com>
2026-04-29 22:55:13 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Fardosa Enow Hussein',
+    author_email= 'firdaushussain56@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
